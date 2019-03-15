383b7a39fc0481229dd1b276fc33c09a9d742d2d
박민호 <pminho0503@hanmail.net>
2019-03-15 17:51:27 +0900

    #10 fixed build error

--- test/test_euler_multiplesOf3and5.cpp
+++ test/test_euler_multiplesOf3and5.cpp
@@ -13,9 +13,9 @@
 class eulerMultipleTest : public CppUnit::TestFixture {
 private:
         CPPUNIT_TEST_SUITE( eulerMultipleTest );
-        CPPUNIT_TEST( test_getSumDivide );
-        CPPUNIT_TEST( test_getSumNum );
-        CPPUNIT_TEST( test_getSumEqu );
+        CPPUNIT_TEST( test_getSumByDivide );
+        CPPUNIT_TEST( test_getSumByNum );
+        CPPUNIT_TEST( test_getSumByEqu );
         CPPUNIT_TEST_SUITE_END();
  
 public:
@@ -23,28 +23,28 @@ public:
  
         void tearDown() {}
  
-        void test_getSumDivide() {
-                CPPUNIT_ASSERT( getSumDivide(10)   == 23);
-                CPPUNIT_ASSERT( getSumDivide(55)   == 698);
-                CPPUNIT_ASSERT( getSumDivide(100)  == 2318);
-                CPPUNIT_ASSERT( getSumDivide(1000) == 233168);
-                CPPUNIT_ASSERT( getSumDivide(6789) == 10749704);
+        void test_getSumByDivide() {
+                CPPUNIT_ASSERT( getSumByDivide(10)   == 23);
+                CPPUNIT_ASSERT( getSumByDivide(55)   == 698);
+                CPPUNIT_ASSERT( getSumByDivide(100)  == 2318);
+                CPPUNIT_ASSERT( getSumByDivide(1000) == 233168);
+                CPPUNIT_ASSERT( getSumByDivide(6789) == 10749704);
         }
  
-        void test_getSumNum() {
-                CPPUNIT_ASSERT( getSumNum(10)   == 23);
-                CPPUNIT_ASSERT( getSumNum(55)   == 698);
-                CPPUNIT_ASSERT( getSumNum(100)  == 2318);
-                CPPUNIT_ASSERT( getSumNum(1000) == 233168);
-                CPPUNIT_ASSERT( getSumNum(6789) == 10749704);
+        void test_getSumByNum() {
+                CPPUNIT_ASSERT( getSumByNum(10)   == 23);
+                CPPUNIT_ASSERT( getSumByNum(55)   == 698);
+                CPPUNIT_ASSERT( getSumByNum(100)  == 2318);
+                CPPUNIT_ASSERT( getSumByNum(1000) == 233168);
+                CPPUNIT_ASSERT( getSumByNum(6789) == 10749704);
         }
 
-        void test_getSumEqu() {
-                CPPUNIT_ASSERT( getSumEqu(10)   == 23);
-                CPPUNIT_ASSERT( getSumEqu(55)   == 698);
-                CPPUNIT_ASSERT( getSumEqu(100)  == 2318);
-                CPPUNIT_ASSERT( getSumEqu(1000) == 233168);
-                CPPUNIT_ASSERT( getSumEqu(6789) == 10749704);
+        void test_getSumByEqu() {
+                CPPUNIT_ASSERT( getSumByEqu(10)   == 23);
+                CPPUNIT_ASSERT( getSumByEqu(55)   == 698);
+                CPPUNIT_ASSERT( getSumByEqu(100)  == 2318);
+                CPPUNIT_ASSERT( getSumByEqu(1000) == 233168);
+                CPPUNIT_ASSERT( getSumByEqu(6789) == 10749704);
         }
 };
  
