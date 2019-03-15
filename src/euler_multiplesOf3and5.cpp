182c9dca98349fa205db1eff1dc68b3eb5cc95b4
박민호 <pminho0503@hanmail.net>
2019-03-15 18:40:09 +0900

    #10 append comments

--- src/euler_multiplesOf3and5.cpp
+++ src/euler_multiplesOf3and5.cpp
@@ -1,5 +1,9 @@
 #include "euler_multiplesOf3and5.h"
 
+/**
+ * Multiples of 3 or 5.
+ * Performance is slow when using divide operator.
+ */
 int getSumByDivide(int toNum) {
 	int sum = 0;
 	for(int i=1; i<toNum; i++) {
@@ -10,6 +14,10 @@ int getSumByDivide(int toNum) {
 	return sum;
 }
 
+/**
+ * Add 3 or 5 to get the value without using the divide.
+ * Performance is faster than using the division operator.
+ */
 int getSumByNum(int toNum) {
 	int v3 = 3;
 	int v5 = 5;
@@ -31,11 +39,22 @@ int getSumByNum(int toNum) {
 	return sum;
 }
 
+/**
+ * Multiples Of n.
+ */
 int getSumByN(int t, int n) {
 	int v = (t-1) / n;
 	return n*(v*(v+1)) / 2;
 }
 
+/**
+ * Calculate multiples of number.
+ * ex) 1000 : 3*(1+2+3+...+333) + 5*(1+2+3+...199) - 15*(1+2+3+..66)
+ *   multiples of 3 : 3*(1+2+3+...+333)
+ *   multiples of 5 : 5*(1+2+3+...+199)
+ *   duplicated multiples of 3 and 5 : 15*(1+2+3+...+66) 
+ * It has the fastest performance than using iterations.
+ */
 int getSumByEqu(int toNum) {
 	return getSumByN(toNum, 3) + getSumByN(toNum, 5) - getSumByN(toNum, 15);
 }
